339a80018d534860acb2e6fa10e5e196f36f35cb
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2013-03-29 14:36:36 +0000

    Mac OSX support - Cocoa based
    git-svn-id: https://svn.code.sf.net/p/vmpk/code/trunk/desktop@388 64c129e3-8119-4a17-9932-72cb407d3134

---
+++ src/maceventhelper.h
@@ -0,0 +1,40 @@
+/*
+    MIDI Virtual Piano Keyboard
+    Copyright (C) 2008-2013, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MACEVENTHELPER_H
+#define MACEVENTHELPER_H
+
+class MacEventHelper
+{
+public:
+    MacEventHelper();
+    ~MacEventHelper();
+    void setNativeEvent(void *p);
+    void clearNativeEvent();
+    bool isKeyPress();
+    bool isKeyRelease();
+    bool isKeyEvent();
+    bool isNotRepeated();
+    int  rawKeyCode();
+
+private:
+    class Private;
+    Private *d;
+};
+
+#endif // MACEVENTHELPER_H
---
+++ src/maceventhelper.mm
@@ -0,0 +1,81 @@
+/*
+    MIDI Virtual Piano Keyboard
+    Copyright (C) 2008-2013, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "maceventhelper.h"
+#include <Cocoa/Cocoa.h>
+
+class MacEventHelper::Private
+{
+public:
+   NSEvent *m_event;
+};
+
+MacEventHelper::MacEventHelper()
+{
+    d = new Private();
+}
+
+MacEventHelper::~MacEventHelper()
+{
+    delete d;
+}
+
+void
+MacEventHelper::setNativeEvent(void *p)
+{
+    d->m_event = static_cast<NSEvent *>(p);
+}
+
+void
+MacEventHelper::clearNativeEvent()
+{
+    d->m_event = 0;
+}
+
+bool
+MacEventHelper::isKeyPress()
+{
+    return (d->m_event != 0) && ([d->m_event type] == NSKeyDown);
+}
+
+bool
+MacEventHelper::isKeyRelease()
+{
+    return (d->m_event != 0) && ([d->m_event type] == NSKeyUp);
+}
+
+bool
+MacEventHelper::isKeyEvent()
+{
+    return (d->m_event != 0) && (([d->m_event type] == NSKeyDown) || ([d->m_event type] == NSKeyUp));
+}
+
+bool
+MacEventHelper::isNotRepeated()
+{
+    return (d->m_event != 0) && ([d->m_event isARepeat] == NO);
+}
+
+int
+MacEventHelper::rawKeyCode()
+{
+    if (d->m_event != 0)
+       return [d->m_event keyCode];
+    return -1;
+}
+
