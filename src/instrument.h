ac364986dcd3b992c224d93eb80a94ec2e67cf58
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2017-08-15 17:11:01 +0000

    release preparations: updated copyright years
    git-svn-id: https://svn.code.sf.net/p/vmpk/code/trunk/desktop@476 64c129e3-8119-4a17-9932-72cb407d3134

--- src/instrument.h
+++ src/instrument.h
@@ -1,9 +1,9 @@
 /*
     MIDI Virtual Piano Keyboard
-    Copyright (C) 2008-2016, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    Copyright (C) 2008-2017, Pedro Lopez-Cabanillas <plcl@users.sf.net>
 
     For this file, the following copyright notice is also applicable:
-    Copyright (C) 2005-2016, rncbc aka Rui Nuno Capela. All rights reserved.
+    Copyright (C) 2005-2017, rncbc aka Rui Nuno Capela. All rights reserved.
     See http://qtractor.sourceforge.net
 
     This program is free software; you can redistribute it and/or modify
