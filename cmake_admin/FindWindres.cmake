17e28eed40f2d046ba8e9d882f40d759be5f87fa
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2019-01-19 20:16:56 +0000

    Copyright ytear updated
    git-svn-id: https://svn.code.sf.net/p/vmpk/code/trunk/desktop@500 64c129e3-8119-4a17-9932-72cb407d3134

--- cmake_admin/FindWindres.cmake
+++ cmake_admin/FindWindres.cmake
@@ -1,5 +1,5 @@
 # Virtual MIDI Piano Keyboard
-# Copyright (C) 2008-2018 Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
+# Copyright (C) 2008-2019 Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
