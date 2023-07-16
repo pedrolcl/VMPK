9131481bf3ea008fa73880b6731afa1004a3e4cc
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2023-07-16 18:43:28 +0200

    Fined ticket [#82] Splash screen removed

--- src/main.cpp
+++ src/main.cpp
@@ -71,24 +71,6 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    QScopedPointer<QSplashScreen> pSplash;
-    if (app.platformName() != QLatin1String("wayland")) {
-        QPixmap px(":/vpiano/vmpk_splash.png");
-        if (app.platformName() != "wayland") {
-            qreal scale = app.primaryScreen()->logicalDotsPerInch() / app.primaryScreen()->physicalDotsPerInch();
-            QSize newsize = px.size() * scale;
-            px = px.scaled(newsize, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
-        }
-        pSplash.reset(new QSplashScreen(px));
-        QFont sf = app.font();
-        sf.setPointSize(20);
-        pSplash->setFont(sf);
-        pSplash->show();
-        app.processEvents();
-        pSplash->showMessage("Virtual MIDI Piano Keyboard " + PGM_VERSION, Qt::AlignBottom | Qt::AlignRight);
-        app.processEvents();
-    }
-
     if (parser.isSet(portableOption) || parser.isSet(portableFileOption)) {
         QString portableFile;
         if (parser.isSet(portableFileOption)) {
@@ -103,16 +85,8 @@ int main(int argc, char *argv[])
     do {
         VPiano w;
         if (w.isInitialized()) {
-            if (!pSplash.isNull()) {
-                pSplash->finish(&w);
-            }
             w.show();
             result_code = app.exec();
-        } else {
-            if (!pSplash.isNull()) {
-                pSplash->close();
-                pSplash.reset();
-            }
         }
     } while (result_code == RESTART_VMPK);
     return result_code;
