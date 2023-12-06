4d8e51db3a4e10df49469eac685f9353dffaced4
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2023-12-06 11:49:29 +0100

    release 0.8.11 preparations
    Build with Qt6 by default.
    For Qt6 compatibility, the USE_QT5 option must be set.
    C++ compiler and runtime requirements upgraded to C++17.
    CMake requirement upgraded to 3.16.

--- cmake_admin/FindWindres.cmake
+++ cmake_admin/FindWindres.cmake
@@ -1,24 +1,39 @@
-# Virtual MIDI Piano Keyboard
-# Copyright (C) 2008-2023 Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along 
-# with this program; If not, see <http://www.gnu.org/licenses/>.
+#[=======================================================================[
+Copyright © 2019-2023 Pedro López-Cabanillas <plcl@users.sf.net>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+* Neither the name of Kitware, Inc. nor the names of its
+  contributors may be used to endorse or promote products derived
+  from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#]=======================================================================]
 
 IF(WINDRES_EXECUTABLE)
     SET(WINDRES_FOUND TRUE)
 ELSE(WINDRES_EXECUTABLE)
     FIND_PROGRAM(WINDRES_EXECUTABLE
-    NAMES windres mingw32-windres i686-mingw32-windres) 
+    NAMES windres mingw32-windres i686-mingw32-windres)
     IF(WINDRES_EXECUTABLE)
         SET(WINDRES_FOUND TRUE)
     ELSE(WINDRES_EXECUTABLE)
@@ -38,7 +53,7 @@ MACRO(ADD_WINDRES_OBJS outfiles)
         GET_FILENAME_COMPONENT(infile ${it} ABSOLUTE)
         SET(outfile ${CMAKE_CURRENT_BINARY_DIR}/${outfile}.obj)
         ADD_CUSTOM_COMMAND(OUTPUT ${outfile}
-            COMMAND ${WINDRES_EXECUTABLE} 
+            COMMAND ${WINDRES_EXECUTABLE}
             ARGS -I${CMAKE_CURRENT_SOURCE_DIR}
                  -i ${infile}
                  -o ${outfile}
